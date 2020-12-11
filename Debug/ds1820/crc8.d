332f9b3f558c134000469af7d19df38c60a3e3c7
Tomi <tomi-best@hotmail.com>
2020-12-11 15:22:28 +0200

    .gitignore fix

--- Debug/ds1820/crc8.d
+++
@@ -1,7 +0,0 @@
-ds1820/crc8.d ds1820/crc8.o: ../ds1820/crc8.c \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
--- freertos/include/portable/MemMang/heap_1.d
+++
@@ -1,5 +0,0 @@
-heap_1.o heap_1.d : ../../Source/portable/MemMang/heap_1.c \
-  ../../Source/include/FreeRTOS.h ../../Source/include/projdefs.h \
-  ../../Source/include/FreeRTOSConfig.h ../../Source/include/portable.h \
-  ../../Source/include/../portable/GCC/ATMega323/portmacro.h \
-  ../../Source/include/task.h ../../Source/include/list.h
